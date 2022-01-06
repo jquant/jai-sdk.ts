0fb0230fe24f9473ac5b898581e64c06941ac7fc
Felipe Esteves <felipegouveiae@gmail.com>
2022-01-06 12:56:38 -0300

    yacks cli tested and validated

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,41 @@
 #!/usr/bin/env node
 
-import {  Authenticator } from './authentication';
+import yargs from "yargs";
 
-const authenticator = new Authenticator();
+const args = process.argv.slice(2);
 
-authenticator.init();
+yargs(args)
+    .command('serve [port]', 'start the server', (yargs) => {
+        return yargs
+            .positional('port', {
+                describe: 'port to bind on',
+                default: 5000
+            })
+    }, (argv) => {
+
+        if (argv.verbose)
+            console.info(`start server on :${argv.port}`)
+
+        console.log(argv.port)
+
+    })
+    .command('scan [port]', 'scan the server port', (yargs) => {
+        return yargs
+            .positional('port', {
+                describe: 'port to bind on',
+                default: 5000
+            })
+    }, (argv) => {
+
+        if (argv.verbose)
+            console.info(`scanning server por :${argv.port}`)
+
+        console.log(argv.port)
+
+    })
+    .option('verbose', {
+        alias: 'v',
+        type: 'boolean',
+        description: 'Run with verbose logging'
+    })
+    .parse()
