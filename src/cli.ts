6037148e891da07ea10b2426d282eff23f1cc316
Felipe Esteves <felipegouveiae@gmail.com>
2022-01-06 09:53:14 -0300

    command line arguments initialized

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,32 @@
 #!/usr/bin/env node
+import {parse} from 'ts-command-line-args';
 
-import {  Authenticator } from './authentication';
+interface CommandLineRequest {
+    path: string
+};
 
-const authenticator = new Authenticator();
+export const args = parse<CommandLineRequest>(
+    {
+        help: {
+            type: Boolean,
+            optional: true,
+            alias: 'h',
+            description: 'Prints this usage guide'
+        },
+    },
+    {
+        helpArg: 'help',
+        headerContentSections: [{
+            header: 'JAI SDK',
+            content: 'Thanks for using JAI SDK.'
+        }],
+        footerContentSections: [{
+            header: 'For further information',
+            content: `Please visit https://jai-sdk.readthedocs.io/en/latest/`
+        }, {
+            header: 'Copyright',
+            content: `2021, JQuant.`
+        }]
+    },
+);
 
-authenticator.init();
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ export const delayMillis = (delayMs: number): Promise<void> => new Promise(resol
 export const greet = (name: string): string => `Hello ${name}`
 
 export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
-  await delayMillis(1000)
-  console.log('done')
-  return true
+    console.log(greet('World'))
+    await delayMillis(1000)
+    console.log('done')
+    return true
 }
