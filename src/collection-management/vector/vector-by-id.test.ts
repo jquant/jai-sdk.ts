0b1100dd0b7647c54d9b804e974e648d8159d309
Felipe Esteves <felipegouveiae@gmail.com>
2022-02-07 06:28:55 -0300

    get vector method implemented

---
+++ src/collection-management/vector/vector-by-id.test.ts
@@ -0,0 +1,134 @@
+// noinspection DuplicatedCode
+
+import {HttpJaiHttpJaiClientGetInterface} from "../../client/http-jai-client-get.interface";
+import {VectorGetter} from "./vector-by.id";
+
+const dummyCollectionName = 'my-collection-name';
+
+class ClientSpy implements HttpJaiHttpJaiClientGetInterface {
+    async get(url: string): Promise<any> {
+        this.urlCalled = url;
+        this.calls += 1;
+
+        if (this.shouldThrow)
+            throw new Error()
+
+        return this.getDummyResult;
+    }
+    urlCalled = '';
+    calls = 0;
+    shouldThrow = false;
+    getDummyResult:any;
+}
+
+const makeSut = () => {
+    const client = new ClientSpy();
+    const sut = new VectorGetter(client);
+
+    return {client, sut};
+}
+
+describe('Vector', () => {
+
+    test('should reject an empty collection name', async () => {
+
+        const {sut} = makeSut();
+
+        await expect(sut.getById('', []))
+            .rejects
+            .toThrow(Error)
+    });
+
+    test('should reject topK lesser than 1', async () => {
+
+        const {sut} = makeSut();
+
+        await expect(sut.getById('', []))
+            .rejects
+            .toThrow(Error)
+    });
+
+    test('should reject any non integer value - string', async () => {
+
+        const {sut} = makeSut();
+        const value: Array<any> = ['home'];
+
+        await expect(sut.getById(dummyCollectionName, value))
+            .rejects
+            .toThrow(Error)
+    });
+
+    test('should reject any non integer value - null', async () => {
+
+        const {sut} = makeSut();
+        const value: Array<any> = [null];
+
+        await expect(sut.getById(dummyCollectionName, value))
+            .rejects
+            .toThrow(Error)
+    });
+
+    test('should perform a request with the array id included and joint', async () => {
+        const {sut, client} = makeSut();
+        const value: Array<any> = [1];
+        const jointValue = value.join(',');
+
+        await sut.getById(dummyCollectionName, value);
+
+        expect(client.urlCalled).toContain(`id=${jointValue}`);
+    });
+
+    test('should perform a request with the array id included, joint and encoded', async () => {
+        const {sut, client} = makeSut();
+        const value: Array<any> = [1];
+        const jointValue = value.join(',');
+        const encodedJointIds = encodeURIComponent(jointValue);
+
+        await sut.getById(dummyCollectionName, value);
+
+        expect(client.urlCalled).toContain(`id=${encodedJointIds}`);
+    });
+
+
+    test('should encode collection name', async () => {
+
+        const {sut, client} = makeSut();
+        const unencoded = 'mY_1 Unenc&%oded collection Nam3';
+        const expected = encodeURIComponent(unencoded);
+
+        await sut.getById(unencoded, [0]);
+
+        expect(client.urlCalled).toContain(expected);
+    });
+
+    test('should perform a request once', async () => {
+        const {sut, client} = makeSut();
+        const value: Array<any> = [1, 2, 3];
+
+        await sut.getById(dummyCollectionName, value);
+
+        expect(client.calls).toBe(1);
+    });
+
+    test('should return client received data', async () => {
+        const {sut, client} = makeSut();
+        const value: Array<any> = [1, 2, 3];
+
+        client.getDummyResult = [10,20,30];
+
+        const received = await sut.getById(dummyCollectionName, value);
+
+        expect(received).toEqual(client.getDummyResult);
+    });
+
+    test('should throw client\'s exception', async () => {
+        const {sut, client} = makeSut();
+        const value: Array<any> = [1, 2, 3];
+
+        client.shouldThrow = true;
+
+        await expect(sut.getById(dummyCollectionName, value))
+            .rejects
+            .toThrow();
+    });
+});
---
+++ src/collection-management/vector/vector-by.id.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata"
+import {inject, injectable} from "tsyringe";
+import {HttpJaiHttpJaiClientGetInterface} from "../../client/http-jai-client-get.interface";
+
+@injectable()
+export class VectorGetter {
+
+    constructor(
+        @inject("HttpJaiHttpJaiClientGetInterface") private readonly client: HttpJaiHttpJaiClientGetInterface
+    ) {
+    }
+
+    async getById(databaseName: string, ids: Array<number>): Promise<void> {
+
+        if (!databaseName)
+            throw new Error('You must provide e databaseName');
+
+        if (!ids || ids.length === 0)
+            throw new Error('The ids are required to perform the search');
+
+        if (ids.some(x => isNaN(x)) || ids.some(x => x == null || false))
+            throw new Error('All the ids must be a number');
+
+        const encodedDatabaseName = encodeURIComponent(databaseName);
+        const joinedIds = ids.join(',');
+        const encodedJoinedIds = encodeURIComponent(joinedIds);
+
+        return await this.client.get(`vector/${encodedDatabaseName}?id=${encodedJoinedIds}`);
+    }
+}
