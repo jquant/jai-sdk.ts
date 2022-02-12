581a25d9628d2251eb2d006dfa673e2654fcd2c5
Felipe Esteves <felipegouveiae@gmail.com>
2022-02-12 20:35:46 -0300

    sample implemented on nodejs

--- samples/nodejs/app.js
+++ samples/nodejs/app.js
@@ -1,10 +1,25 @@
 const jai = require('jai-sdk-testing');
 
-// const { GetStatus } = jai;
+const { GetStatus, Authenticate } = jai;
 
 const express = require('express')
 const app = express()
 const port = 3000
+
+
+app.get('/authenticate/:key', (req, res) => {
+    Authenticate(req.params.key);
+    res.send('Authenticated Successfuly!');
+})
+
+app.get('/get-status/:key', (req, res) => {
+
+    Authenticate(req.params.key);
+
+    GetStatus().then(data => {
+        res.send(data);
+    })
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,3 +28,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
