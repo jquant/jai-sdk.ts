1cd0923d313f2c2d3f62c06184404f0f2856c805
Felipe Esteves <felipegouveiae@gmail.com>
2022-05-19 12:32:09 -0300

    sample updated with JAI logo

--- samples/react-js-sample/src/App.js
+++ samples/react-js-sample/src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.png';
 import './App.css';
+// import { authenticate, similaritySearchById } from 'jai-sdk';
+
+const { REACT_APP_JAI_SDK_KEY } = process.env;
 
 function App() {
+
+  function getApiKey() {
+    if (!REACT_APP_JAI_SDK_KEY)
+      return "Please, set the REACT_APP_JAI_SDK_KEY in your env.local file";
+
+    const start = REACT_APP_JAI_SDK_KEY.substring(0, 4);
+    const end = REACT_APP_JAI_SDK_KEY.substring(28, 32);
+    return `${start}***********************${end}`;
+  }
+
+
+  function executeSimilaritySearch() {
+    // authenticate(REACT_APP_JAI_SDK_KEY);
+    // similaritySearchById("productImages", [10000], 10).then(data => {
+    //   console.log(data);
+    // })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>          Your API KEY        </p>
+        <small>
+          {getApiKey()}
+        </small>
+        <a className="App-link" href="https://getjai.com/" target="_blank" rel="noopener noreferrer"        >
+          More About JAI
         </a>
+
+        <div>
+          <button onClick={executeSimilaritySearch()}>
+            Similar By ID
+          </button>
+        </div>
+
       </header>
+
+
     </div>
   );
 }
