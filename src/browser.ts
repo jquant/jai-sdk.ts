f7d9e45efcae115c55658c4d43ddd2b57306a58b
Felipe Esteves <felipegouveiae@gmail.com>
2022-05-19 12:52:52 -0300

    import "reflect-metadata" added to browser file

--- src/browser.ts
+++ src/browser.ts
@@ -2,7 +2,7 @@
  * This file is the entrypoint of browser builds.
  * The code executes when loaded in a browser.
  */
-
+import "reflect-metadata"
 import {Initializer} from "./ioc/register";
 
 Initializer.initializeInversionOfControl();
