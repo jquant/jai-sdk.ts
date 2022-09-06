36b05425a9cd8207643411ec0cca916135e0c60d
Matheus vinicius xavier <mv.xavier_@hotmail.com>
2022-09-06 12:59:46 -0300

    jai api getfields return adjusts - handler for array and object

--- src/collection-management/table-fields/get-table-fields.ts
+++ src/collection-management/table-fields/get-table-fields.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import {inject, injectable} from "tsyringe";
 import {HttpJaiClientGetInterface} from "../../client/http-jai-client-get.interface";
+import {JaiGetFieldsNotSupportedException} from "../../exceptions/JaiGetFieldsNotSupportedException";
 
 export interface GetTableFieldsClient {
     fields(databaseName: string): Promise<any>;
@@ -24,7 +25,22 @@ export class GetTableFields implements GetTableFieldsClient {
         if (!databaseName)
             throw new Error('You must provide e databaseName');
 
-        const {fields} = await this.client.get(`fields/${databaseName}`);
-        return fields;
+        const result = await this.client.get(`fields/${databaseName}`);
+
+        return this.extractFields(result);
+    }
+
+    private extractFields(result: any) {
+        if (Object.keys(result).includes('fields')) {
+            return result.fields;
+        }
+
+        if (Array.isArray(result) && result.length > 0) {
+            if (Object.keys(result[0]).includes('fields')) {
+                return result[0].fields;
+            }
+        }
+
+        throw new JaiGetFieldsNotSupportedException("Result structure not supported");
     }
 }
